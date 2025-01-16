cb033bd675bb82520f1fa6c59ed3cc13626c5867
siddharthmohite <siddharthmohite409@gmail.com>
2025-01-15 20:20:23 -0800

    Cleaning up functionality file and structured the code

--- tests/functionality.py
+++ tests/functionality.py
@@ -9,11 +9,8 @@ from dotenv import load_dotenv
 # Load environment variables from a .env file
 load_dotenv()
 
-def test_qdrant_collection():
-    """
-    Test creating a real Qdrant collection using the library.
-    """
-    # Load Qdrant credentials from environment variables
+
+def get_qdrant_settings():
     qdrant_api_key = os.getenv("QDRANT_API_KEY_REMOTE")
     qdrant_endpoint = os.getenv("QDRANT_ENDPOINT_REMOTE")
     # Initialize the factory for Qdrant
@@ -22,59 +19,66 @@ def test_qdrant_collection():
         api_key=qdrant_api_key,
         endpoint=qdrant_endpoint,
     )
-    name = "test_qdrant_collection"
-    # Create a collection
-    try:
-        db.delete_collection(name)
-        # db.get_collection_details(name)
-        # db.list_all_collections()
-        # db.create_collection(
-        #     name="test_qdrant_collection",
-        #     dimension=128,
-        #     distance=Distance.COSINE,  # Qdrant-specific distance metric
-        # )
-        # print("Qdrant: Successfully created the collection 'test_qdrant_collection'.")
-    except Exception as e:
-        print(f"Qdrant: Failed to delete collection with {name}: {e}")
+    return db
 
-
-def test_pinecone_collection():
-    """
-    Test creating a real Pinecone collection using the library.
-    """
-    # Load Pinecone credentials from environment variables
+def get_pinecone_settings():
     pinecone_api_key = os.getenv("PINECONE_API_KEY_REMOTE")
     # Initialize the factory for Pinecone
     db = VectorDBFactory(
         db_type="pinecone",
         api_key=pinecone_api_key,
     )
-    # try:
-    #     db = PineConeDB()
+    return db
 
-    #     db.create_collection(
-    #         name="testing",
-    #         dimension=128,
-    #         metric="cosine",
-    #         cloud="aws",
-    #         region="us-east-1"
-    #     )
-    # except Exception as e:
-    #     print(f"Pinecone: Failed to create the collection: {e}")
-    #     return
-    name = "testing"
-    try:
-        # db.get_collection_details(name)
-        # db.list_all_collections()
-        db.delete_collection(name)
-    except Exception as e:
-        print(f"Pinecone: Failed to Delete collection with {name}: {e}")
-        return
-    
+db_qdrant  = get_qdrant_settings()
+db_pinecone = get_pinecone_settings()
+
+def test_create_collection():
+    """
+    Testing create collection method qdrant and pinecone
+    """
+    db_qdrant.create_collection(
+                name = "Siddharth's testing",
+                dimension = 128,
+                distance = Distance.COSINE,
+    )
+
+    db_pinecone.create_collection(
+         name="testing",
+            dimension=128,
+            metric="cosine",
+            cloud="aws",
+            region="us-east-1"
+    )
+
+def test_get_collection_details():
+    """
+     Testing Get collection details
+    """
+    name_qdrant ="testing"
+    name_pinecone = "testing"
+    db_qdrant.get_collection_details(name_qdrant)
+    db_pinecone.get_collection_details(name_pinecone)
+
+def test_list_all_collections():
+    db_qdrant.list_all_collections()
+    db_pinecone.list_all_collections()
+           
+def test_delete_collection():
+    """
+    Testing delete a collection
+    """
+    name_qdrant = "testing"
+    name_pinecone = "testing"
+    db_qdrant.delete_collection(name_qdrant)
+    db_pinecone.delete_collection(name_pinecone)
 
 if __name__ == "__main__":
-    print("Deleting a collection from Qdrant")
-    test_qdrant_collection()
-
-    print("\nDeleting a collection(index) from Pinecone")
-    test_pinecone_collection()
+    # Create Collection
+    test_create_collection()
+    # Get Collection details
+    test_get_collection_details()
+    # List All collections
+    test_list_all_collections()
+    # Delete a collection
+    test_delete_collection()
